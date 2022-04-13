7f793233840640ee9faf2ccacd970b3e2ddf53fd
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-13 14:38:52 +0200

    second teste created

--- isPalindrome.test.js
+++ isPalindrome.test.js
@@ -3,20 +3,21 @@ const isPalindrome = require("./isPalindrome");
 // Given / when / then
 
 describe("Given the funnction isPalindrome", () => {
-  // describe("When it recieves an empty string", () => {
-  //   test("Then it should throw the error 'Text not provided'", () => {
-  //     const emptyString = "";
-
-  //     const chechIfEmpty = isPalindrome(emptyString);
-
-  //     expect(chechIfEmpty).toThrow("Text not provided");
-  //   });
-  // });
-
   describe("When it recieves the text 'Ana'", () => {
     test("Then it should return true", () => {
       const introducedString = "Ana";
       const expctedResult = true;
+
+      const checkTest = isPalindrome(introducedString);
+
+      expect(checkTest).toBe(expctedResult);
+    });
+  });
+
+  describe("When it recieves the text 'Roma'", () => {
+    test("Then it should return false", () => {
+      const introducedString = "Roma";
+      const expctedResult = false;
 
       const checkTest = isPalindrome(introducedString);
 
